47023101f456d7416f7700ef46bafcb697f46fe4
arnicauser01 <arnicauser01@outlook.com>
2026-03-03 19:41:48 -0500

    Update vul.sh

--- vul.sh
+++ vul.sh
@@ -3,6 +3,7 @@ echo "Running insecure script..."
 # Insecure hardcoded password
 PASSWORD="admin123"
 echo "Password is $PASSWORD"
-
+# Agentic Rule (ARNIE_ACCESS_PERMISSION): Require admin role to for file download
 # Running as root (bad practice)
 id
+# Agentic Rule (ARNIE_ACCESS_PERMISSION): Require admin role to for file download
